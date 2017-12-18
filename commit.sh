e8fd7edeea2f0138e8a169a4b25f35bd9d8bea92
neumond <knifeslaughter@gmail.com>
2017-12-18 22:50:25 +0300

    Switched to symlinked directory

--- commit.sh
+++
@@ -1,6 +0,0 @@
-cd "$(dirname "$0")"
-FOLDER="/home/play/.minecraft_1.12.2_mods/saves/azaza/opencomputers/4ace2503-2838-4bc9-a6af-1119761b59b8"
-
-rm -R ./items/
-mkdir items
-cp -R "${FOLDER}/." items/
