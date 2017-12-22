e97acfc8f6417e5fac3a94b3f0f4c22df11e540c
neumond <knifeslaughter@gmail.com>
2017-12-22 03:43:24 +0300

    More testing for UI

--- items/try.lua
+++ items/try.lua
@@ -343,6 +343,49 @@ local function tokenizeMarkup(gpu)
 end
 
 
+local function scrollingHandler(root, sm)
+    local event = require("event")
+    repeat
+        local _, _, _, key = event.pull("key_down")
+        local sx, sy = 0, 0
+        repeat
+            if key == 200 then
+                sy = sy - 1
+            elseif key == 208 then
+                sy = sy + 1
+            elseif key == 203 then
+                sx = sx - 1
+            elseif key == 205 then
+                sx = sx + 1
+            end
+            local _, _, _, key = event.pull(0, "key_down")
+        until key == nil
+        if (sx ~= 0) or (sy ~= 0) then
+            sm:relativeScroll(sx, sy)
+        end
+        -- root:update()
+        -- 200 up
+        -- 208 down
+        -- 203 left
+        -- 205 right
+    until key == 28
+end
+
+
+local function runUsingRealGpu(f)
+    local gpu = require("component").gpu
+    local oldForeground = gpu.getForeground()
+    local oldBackground = gpu.getBackground()
+    gpu.setBackground(0x202020)
+    gpu.fill(1, 1, 200, 100, " ")
+
+    f(gpu)
+
+    gpu.setForeground(oldForeground)
+    gpu.setBackground(oldBackground)
+end
+
+
 local function renderingUI()
     local function renderFrames(gpu)
         local root, mfs = createUI(gpu)
@@ -368,49 +411,95 @@ local function renderingUI()
     end
 
     local function execGpu()
-        local gpu = require("component").gpu
-
-        local oldForeground = gpu.getForeground()
-        local oldBackground = gpu.getBackground()
-        gpu.setBackground(0x202020)
-        gpu.fill(1, 1, 200, 100, " ")
-
-        local root, sm = renderFrames(gpu)
-
-        -- waitForKey()
-
-        local event = require("event")
-        repeat
-            local _, _, _, key = event.pull("key_down")
-            local sx, sy = 0, 0
-            repeat
-                if key == 200 then
-                    sy = sy - 1
-                elseif key == 208 then
-                    sy = sy + 1
-                elseif key == 203 then
-                    sx = sx - 1
-                elseif key == 205 then
-                    sx = sx + 1
-                end
-                local _, _, _, key = event.pull(0, "key_down")
-            until key == nil
-            if (sx ~= 0) or (sy ~= 0) then
-                sm:relativeScroll(sx, sy)
-            end
-            -- root:update()
-            -- 200 up
-            -- 208 down
-            -- 203 left
-            -- 205 right
-        until key == 28
-
-        gpu.setForeground(oldForeground)
-        gpu.setBackground(oldBackground)
+        runUsingRealGpu(function(gpu)
+            local root, sm = renderFrames(gpu)
+            -- waitForKey()
+            scrollingHandler(root, sm)
+        end)
     end
 
     outsideOC()
     -- execGpu()
+end
+
+
+local function scrollingInCentralFrame()
+    runUsingRealGpu(function(gpu)
+        local root = require("ui.windows").FrameRoot(gpu)
+        local text, styles = mu5(require("ui.markup"))
+
+        local mfs = {}
+        do
+            for i=1,9 do
+                table.insert(mfs, root:Markup(text, styles))
+            end
+            for _, mf in ipairs(mfs) do
+                mf:setMinimalContentWidth(70)
+            end
+
+            local c = root:VSplit()
+            c:setBorderType(2)
+            for row=1,3 do
+                local h = root:HSplit()
+                h:setBorderType(1)
+                for col=1,3 do
+                    h.children:append(mfs[col * 3 - 3 + row])
+                end
+                c.children:append(h)
+            end
+
+            root:assignRoot(c)
+        end
+
+        root:update()
+        scrollingHandler(root, mfs[5])
+    end)
+end
+
+
+local function veryLongLipsum()
+    local m = require("ui.markup")
+    local lipsum = {
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at tortor metus. Nam vel pellentesque orci. Morbi eu eros diam. Morbi malesuada magna et lectus lacinia elementum. Quisque suscipit, leo scelerisque interdum eleifend, ligula sem dignissim nisi, eu volutpat sem mauris id est. Morbi venenatis molestie ligula, in vestibulum tellus eleifend vitae. Donec arcu metus, gravida eget massa et, egestas mattis nunc. Nulla at posuere magna, et viverra ipsum. Nullam eleifend urna urna, in iaculis est fermentum ac. Aliquam erat volutpat. Sed maximus augue et odio vulputate imperdiet. Aliquam dignissim varius malesuada. Sed iaculis nisl et nisi imperdiet, nec suscipit nunc cursus. Proin finibus auctor lacus.",
+        "Vivamus pulvinar ullamcorper scelerisque. Nunc egestas est est, in euismod ligula posuere scelerisque. Vivamus in suscipit elit, ac cursus felis. Donec sem dui, molestie finibus pellentesque nec, pulvinar ac purus. Maecenas augue enim, ultricies id gravida a, sodales faucibus nunc. Maecenas fermentum porttitor felis, vel mollis ligula blandit vitae. Maecenas aliquet felis et velit condimentum, in bibendum mauris iaculis. Cras ex ligula, pretium eget velit at, sodales eleifend enim. Donec commodo blandit urna, a gravida mi maximus id. Vestibulum sed tortor vehicula, faucibus sapien in, sodales turpis. Quisque diam dui, eleifend eget ullamcorper a, consectetur sed urna.",
+        "Etiam nisi velit, consectetur vitae lacus eu, eleifend elementum libero. Nunc efficitur mi sed neque molestie sodales. Vivamus ac magna eros. Vestibulum eu vulputate neque. Praesent eu turpis vel orci dictum dignissim sed a nisi. Nulla ante augue, lobortis nec pretium a, pretium vel ex. Fusce vehicula ac orci ac feugiat. Sed molestie ex elit, ac semper dolor imperdiet eget. Phasellus nec vestibulum turpis. Fusce accumsan leo at fermentum eleifend. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc dolor est, viverra ac auctor a, tincidunt et leo. Duis eleifend, nisi sit amet ullamcorper molestie, risus ligula gravida lectus, dictum consequat velit elit sit amet libero. Nunc nec odio eu eros gravida tempor. Mauris vel tortor ut velit tincidunt congue a fringilla urna. Aliquam laoreet felis id neque lobortis, nec consequat justo euismod.",
+        "Fusce vestibulum tellus enim. In vel mauris neque. Vivamus sodales iaculis nunc in volutpat. In hac habitasse platea dictumst. Nullam sit amet diam ac nunc ullamcorper imperdiet. Vivamus vel sem nec orci efficitur euismod ac nec nisl. Morbi tristique velit eget libero hendrerit varius. Praesent at diam pulvinar lorem lobortis feugiat commodo id leo. Vestibulum interdum ligula id dictum porttitor. Sed ut ante volutpat, fringilla libero eu, dignissim nisi. Donec fermentum lorem mauris. Morbi vitae dignissim nulla. Ut vel bibendum neque, et ultrices enim. Integer id rutrum ex. Aliquam aliquam accumsan tortor a blandit.",
+        "Nullam cursus vitae velit non imperdiet. Cras commodo vel felis condimentum dignissim. Cras sagittis ipsum aliquet ipsum condimentum maximus. Nulla mattis erat eu purus condimentum molestie. Mauris orci lacus, mattis vitae turpis vitae, blandit hendrerit lacus. Nulla facilisi. Pellentesque placerat neque non urna egestas, ac sodales risus accumsan. Curabitur arcu velit, eleifend a arcu ut, ultrices sagittis eros. Morbi in erat a velit faucibus dictum ac sit amet justo. Maecenas sollicitudin eros vel blandit accumsan. Ut eleifend neque et ligula pharetra fringilla.",
+        "Mauris quis nulla sed felis convallis suscipit eu sit amet orci. Nam nec mi justo. Vestibulum id dignissim velit, eget bibendum magna. Nullam sed diam ullamcorper, venenatis libero et, faucibus risus. Duis ac ligula sed felis vulputate finibus vel sit amet dui. Cras placerat finibus nunc non sagittis. Pellentesque id metus eu arcu finibus feugiat sed nec nisl.",
+        "Cras a consectetur lorem. Vestibulum id lacus mattis, tristique nibh ut, scelerisque justo. Nullam vulputate fermentum sem et consequat. Vestibulum quis neque non erat bibendum tempus. Pellentesque sollicitudin condimentum nisi, eu imperdiet turpis cursus et. Nulla et commodo dolor. Aenean molestie eros sed mauris placerat sagittis.",
+        "Vivamus ut ullamcorper ex. Integer aliquet sem ac bibendum sollicitudin. Praesent vitae fringilla lectus, ac placerat ipsum. Aliquam cursus lectus id lacus euismod, in suscipit quam placerat. Suspendisse fermentum, nunc eu vestibulum tincidunt, dui velit sollicitudin justo, eget posuere enim eros sed tellus. Phasellus magna velit, tristique nec tempor eu, luctus ac mauris. Maecenas fermentum feugiat ante id gravida. Quisque luctus nulla eget massa molestie iaculis. Nullam placerat posuere elit, in condimentum purus vehicula eu. Donec luctus interdum lectus, a accumsan libero dictum ac. Sed gravida at mauris id finibus.",
+        "Etiam iaculis rutrum eros et faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam ornare ullamcorper odio nec pellentesque. Nunc eros purus, interdum nec dignissim ut, cursus sed velit. Sed ultrices, nisi vel volutpat consequat, enim erat laoreet mi, molestie aliquet felis tortor vitae velit. Nunc vulputate, augue in tempor ornare, leo metus mollis felis, a fermentum nibh velit eu lectus. Suspendisse mattis auctor elementum. Praesent hendrerit, diam at commodo egestas, lorem est ullamcorper sapien, sed imperdiet massa leo eget nisi. Pellentesque efficitur, neque nec ultrices mattis, erat eros viverra massa, quis ultricies dolor orci at nisi. Morbi pellentesque sed turpis ac consequat. Vestibulum fermentum turpis posuere lorem tincidunt, eu aliquet sapien tincidunt. Sed a sapien vehicula, laoreet purus non, euismod leo. Vivamus in rhoncus neque.",
+        "Suspendisse eget placerat nulla. Pellentesque mi nisl, euismod vitae orci ac, volutpat pharetra magna. Aliquam ut fringilla tellus. Nunc et lorem ac risus viverra tempor. Ut massa sem, rhoncus vel enim sed, dictum efficitur arcu. Nulla facilisi. Duis in lorem ut tellus iaculis condimentum. Nulla facilisi."
+    }
+    local divs = {}
+    for i=1,3 do
+        for _, line in ipairs(lipsum) do
+            local words = {}
+            for word in line:gmatch("%S+") do
+                table.insert(words, word)
+            end
+            if #divs > 0 then
+                table.insert(divs, m.Div("* * *"):class("hr"))
+            end
+            table.insert(divs, m.Div(table.unpack(words)))
+        end
+    end
+    local text = m.Div(table.unpack(divs))
+    return text, {
+        m.Selector({"hr"}, {align="center"})
+    }
+end
+
+
+local function renderBigWallOfText()
+    runUsingRealGpu(function(gpu)
+        local root = require("ui.windows").FrameRoot(gpu)
+        local text, styles = veryLongLipsum()
+        local sm = root:Markup(text, styles)
+        root:assignRoot(sm)
+        root:update()
+        scrollingHandler(root, sm)
+    end)
 end
 
 
@@ -423,4 +512,6 @@ end
 -- coros()
 -- multiParams()
 -- tokenizeMarkup()
-renderingUI()
+-- renderingUI()
+-- scrollingInCentralFrame()
+renderBigWallOfText()
