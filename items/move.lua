ba42c79c776fd3f7c93a359cbbca72c1622f328b
neumond <knifeslaughter@gmail.com>
2018-01-06 23:59:45 +0300

    Path encoder

--- items/move.lua
+++ items/move.lua
@@ -2,34 +2,99 @@ local robot = require("robot")
 local keyboard = require("keyboard")
 
 
-local function main()
-    local keySwitch = {
-        [keyboard.keys.up]=function()
-            robot.forward()
-        end,
-        [keyboard.keys.down]=function()
-            robot.back()
-        end,
-        [keyboard.keys.left]=function()
-            robot.turnLeft()
-        end,
-        [keyboard.keys.right]=function()
-            robot.turnRight()
-        end,
-        [keyboard.keys.z]=function()
-            robot.up()
-        end,
-        [keyboard.keys.x]=function()
-            robot.down()
-        end
-    }
+local reverseActions = {
+    forward="back",
+    back="forward",
+    turnLeft="turnRight",
+    turnRight="turnLeft",
+    up="down",
+    down="up"
+}
 
+
+local function doAction(logData, action)
+    if robot[action]() then
+        table.insert(logData, action)
+    end
+end
+
+
+local function reverseAction(logData)
+    if #logData <= 0 then return end
+    local action = logData[#logData]
+    if robot[reverseActions[action]] then
+        logData[#logData] = nil
+    end
+end
+
+
+local keyMap = {
+    up="forward",
+    down="back",
+    left="turnLeft",
+    right="turnRight",
+    z="up",
+    x="down"
+}
+local keyMap2 = {}
+for k, v in pairs(keyMap) do
+    keyMap2[keyboard.keys[k]] = v
+end
+
+
+local encodeTable = {
+    forward="F",
+    back="B",
+    turnLeft="L",
+    turnRight="R",
+    up="U",
+    down="D"
+}
+
+
+local function encode(logData)
+    local result = {}
+    local lastAction
+    local counter = 0
+
+    local function flush()
+        if counter <= 0 then return end
+        if counter == 1 then
+            table.insert(result, lastAction)
+        end
+        table.insert(result, lastAction .. counter)
+        counter = 0
+    end
+
+    for _, action in ipairs(logData) do
+        if action ~= lastAction then
+            flush()
+            lastAction = action
+        else
+            counter = counter + 1
+        end
+    end
+    flush()
+
+    return table.concat(result)
+end
+
+
+local function main()
+    local logData = {}
     local event = require("event")
     repeat
         local _, _, _, key = event.pull("key_down")
-        local f = keySwitch[key]
-        if f ~= nil then f() end
+        local action = keyMap2[key]
+        if action ~= nil then
+            doAction(logData, action)
+        end
+        if key == keyboard.keys.back then
+            reverseAction(logData)
+        end
     until key == keyboard.keys.enter
+    print(encode(logData))
+    return logData
 end
 
 
